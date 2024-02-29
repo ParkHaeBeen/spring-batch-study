d72f6ea32ae1395be23ccedf744f4c7eb0dfa91a
haebeen <haebing0309@gmail.com>
2024-02-29 17:39:57 +0900

    부분 수정

--- src/main/java/com/example/springbatchstudy/SimpleJobConfig.java
+++ src/main/java/com/example/springbatchstudy/SimpleJobConfig.java
@@ -1,14 +1,11 @@
 package com.example.springbatchstudy;
 
-
-
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
